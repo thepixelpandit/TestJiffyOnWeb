9bab8996f7d5c3f6dc602df0f1757b448a9626e1
Aashish Mittal <149008767+thepixelpandit@users.noreply.github.com>
2023-10-26 17:01:41 +0530

    Update app. Removed DB URL variable.js

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ const { dataSorter, toTitleCase } = require("./public/importantFunctions.js");
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_Database_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
