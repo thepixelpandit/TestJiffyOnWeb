db52c8a89514d9c7003cb3aaeed8ba000d6a2a6b
Aashish Mittal <149008767+thepixelpandit@users.noreply.github.com>
2023-10-26 16:58:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const { dataSorter, toTitleCase } = require("./public/importantFunctions.js");
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_Database_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
@@ -73,7 +74,7 @@ app.get("/forgotpassword(.html)?", (req, res) => {
 app.get("/timelogger(.html)?", (req, res) => {
   console.log(userAlive);
   if (userAlive) {
-    serveHTMLFile(req, res, "timelogger.html");
+    serveHTMLFile(req, res, "timeLogger.html");
   } else {
     res.send(
       '<script>alert("You are not authorized to view this page.!"); window.location.href = "/";</script>'
